d4a171cbd3e2e1012d73c28ba30cc7a858294407
triandamai <triannurizkillah@gmail.com>
2021-04-25 23:04:59 +0700

    feat:add api

--- app/db/db-models.js
+++ app/db/db-models.js
@@ -9,6 +9,17 @@ export const UserSchema = new mongoose.Schema(
   { versionKey: false }
 );
 
-export const TestSchema = new mongoose.Schema({});
+export const TestSchema = new mongoose.Schema({
+  title: { type: String },
+  description: { type: String },
+  thumbnail: { type: String },
+  soal: { type: Array }
+});
 
-export const SoalSchema = new mongoose.Schema({});
+export const SoalSchema = new mongoose.Schema({
+  question: { type: String },
+  image: { type: String },
+  soal_type: { type: Number },
+  choise: { type: Array },
+  answer: { type: String }
+});
